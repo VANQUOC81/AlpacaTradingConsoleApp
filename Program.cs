d2c275a2125fbb20dc566d19a279a5437c83e65b
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-12-15 21:43:41 +0100

    implement logic to get next open en next close market

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Alpaca.Markets;
+using System.Threading.Tasks;
+
+namespace AlpacaExample
+{
+    internal static class Program
+    {
+        private const String KEY_ID = "PKUPOVPGCHJIVR5B0BEC";
+
+        private const String SECRET_KEY = "kheLqZujdbPtIdv9x6i9pGfG8IveBBewaGJlNksT";
+
+        public static async Task Main()
+        {
+            var client = Environments.Paper
+                .GetAlpacaTradingClient(new SecretKey(KEY_ID, SECRET_KEY));
+
+            var clock = await client.GetClockAsync();
+
+            if (clock != null)
+            {
+                Console.WriteLine(
+                    "Timestamp: {0}, NextOpen: {1}, NextClose: {2}",
+                    clock.TimestampUtc, clock.NextOpenUtc, clock.NextCloseUtc);
+            }
+        }
+    }
+}
